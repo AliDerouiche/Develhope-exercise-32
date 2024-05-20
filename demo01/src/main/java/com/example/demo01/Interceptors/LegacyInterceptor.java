8349a23aef5ad37e3ab041ab61c5e7444bf4557b
Bisco <alidero1605@gmail.com>
2024-05-20 07:42:31 +0200

    Initial commit

---
+++ demo01/src/main/java/com/example/demo01/Interceptors/LegacyInterceptor.java
@@ -0,0 +1,24 @@
+package com.example.demo01.Interceptors;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.Nullable;
+
+
+@Component
+public class LegacyInterceptor implements HandlerInterceptor {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(request.getRequestURL().toString().contains("/legacy")) {
+            response.setStatus(HttpServletResponse.SC_GONE);
+            return false;
+        } return true;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+    }
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    }
+}
---
+++ demo01/src/main/java/com/example/demo01/configuration/SpringMVCConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.demo01.configuration;
+
+
+import com.example.demo01.Interceptors.APILoggingInterceptor;
+import com.example.demo01.Interceptors.LegacyInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class SpringMVCConfiguration implements WebMvcConfigurer {
+    @Autowired
+    APILoggingInterceptor apiLoggingInterceptor;
+
+    @Autowired
+    LegacyInterceptor legacyInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiLoggingInterceptor);
+        registry.addInterceptor(legacyInterceptor);
+    }
+}
